38a123201e2ca03869560ef154d6b27c6785eb19
Gabriel <105232254+GabrielGTzin@users.noreply.github.com>
2024-07-25 11:32:36 -0300

    Add files via upload

---
+++ Missao ia/script.js
@@ -0,0 +1,12 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "Pergunta1", 
+    alternativas: ["Alternativa11", ]
+    "Pergunta2"
+
+]
